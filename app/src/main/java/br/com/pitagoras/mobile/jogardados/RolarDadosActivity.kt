422ebd8178fbffb6319c530cbd96978a17400886
Fabricio Valadares <fabricio.valadares@gmail.com>
2021-09-10 20:28:53 -0300

    Criação do layout da da segunda tela

---
+++ app/src/main/java/br/com/pitagoras/mobile/jogardados/RolarDadosActivity.kt
@@ -0,0 +1,11 @@
+package br.com.pitagoras.mobile.jogardados
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RolarDadosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rolar_dados)
+    }
+}
