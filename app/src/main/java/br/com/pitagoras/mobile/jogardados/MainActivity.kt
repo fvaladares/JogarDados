222feaa088c9958270ba49af6d778b733dd0d897
Fabricio Valadares <fabricio.valadares@gmail.com>
2021-09-10 21:37:40 -0300

    Passagem de parâmetros para a segunda activity.
    Exemplo de como ocultar um elemento da tela.
    
    Exemplo de uso da função geradora de números aleatórios.

--- app/src/main/java/br/com/pitagoras/mobile/jogardados/MainActivity.kt
+++ app/src/main/java/br/com/pitagoras/mobile/jogardados/MainActivity.kt
@@ -17,12 +17,51 @@ package br.com.pitagoras.mobile.jogardados
 ~ limitations under the License.
 -->
  */
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.RadioButton
 import androidx.appcompat.app.AppCompatActivity
+import br.com.pitagoras.mobile.jogardados.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        configurarComponentes()
+    }
+
+    private fun configurarComponentes() {
+        binding.buttonVamosLa.setOnClickListener {
+            abrirNovaTela()
+        }
+    }
+
+    private fun abrirNovaTela() {
+        val idItemSelecionado = binding.radioGroupQuantidadeDados.checkedRadioButtonId
+        val radioButtonSelecionado = findViewById<RadioButton>(idItemSelecionado)
+        Log.d("[PITAGORAS]", "Id item selecionado: $idItemSelecionado")
+        Log.d(
+            "[PITAGORAS]",
+            "Rótulo do botão selecionado: " +
+                    "${radioButtonSelecionado?.text ?: "Nenhum item foi seleciondo"}"
+        )
+        // TODO(Informar ao usuário que ele deve selecionar a quantidade de dados, e não abrir a outra activity.)
+        // TODO(Conferir se o objeto radioButtonselecionado não é nulo antes de realizar a conversão para inteiro na linha 58)
+        val intent = Intent(this, RolarDadosActivity::class.java).apply {
+            putExtra(
+                TAG,
+                if (radioButtonSelecionado.text.equals(getString(R.string.rotulo_um_dado))) 1 else 2
+            )
+        }
+        startActivity(intent)
+    }
+
+    companion object {
+        const val TAG = "qtdDados"
     }
 }
--- app/src/main/java/br/com/pitagoras/mobile/jogardados/RolarDadosActivity.kt
+++ app/src/main/java/br/com/pitagoras/mobile/jogardados/RolarDadosActivity.kt
@@ -1,11 +1,54 @@
 package br.com.pitagoras.mobile.jogardados
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import br.com.pitagoras.mobile.jogardados.databinding.ActivityRolarDadosBinding
+import kotlin.random.Random
 
 class RolarDadosActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRolarDadosBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_rolar_dados)
+        binding = ActivityRolarDadosBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        configurarComponentes()
+    }
+
+    private fun configurarComponentes() {
+        // Capituramos dados vindos da outra activity (1, caso nada seja enviado)
+        var quantidadeDados = intent.getIntExtra(MainActivity.TAG, 1)
+        Log.d("[PITAGORAS]", "Quantidade de dados passada: $quantidadeDados")
+        configurarBotoes(quantidadeDados)
+
+        // Exemplo de como remover um elemento da view
+        binding.ivDado2.visibility = View.GONE
+    }
+
+    private fun configurarBotoes(quantidadeDados: Int) {
+        binding.btRolarDados.setOnClickListener {
+            rolarDados(quantidadeDados)
+        }
+    }
+
+    // TODO(Utilizar a quantidade de dados para determinar a quantidade de números aleatóris gerados e também quantos dados serão exibidos)
+    private fun rolarDados(quantidadeDados: Int) {
+        val resultado = Random.nextInt(6) + 1
+
+        val faceDoDado = when (resultado) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Altera a imagem exibida de acordo com o comando da linha 37 (seleção de imagem)
+        binding.ivDado1.setImageResource(faceDoDado)
+
     }
 }
